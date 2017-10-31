c61f698cbd154d64f623e7d905e58113c3978a88
Louis Atkin <32098761+Atkin-L-BPC-Magenta@users.noreply.github.com>
2017-10-31 13:41:23 +0000

    PHP file on elseif

---
+++ PHP file on elseif.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html> 
+ <html> 
+ <head> 
+ <title>test 01</title> 
+ </head> 
+ <body> 
+ <?php 
+ $fName = "Ross"; 
+ $sName = "Guy"; 
+ $fullname = $fName . " " . $sName; 
+  
+ echo $fullname; 
+  
+ if ($fullname == "Rupert") { 
+ echo "holy moly, thats a great name"; 
+ } 
+ elseif 
+ ($fullname == "Ross Guy") { 
+ echo "hey wait a minute, you have studied the art of wing chun - cool! "; 
+ } 
+ else 
+ { 
+ echo "Who are ya?"; 
+ }  
+  
+ ?> 
+ </body> 
+ </html> 
